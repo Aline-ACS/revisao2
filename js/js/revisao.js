b017062e2bd760ecf6c75f25ac22c0b4e5277972
Aline-Acs <aline_capoani@hotmail.com>
2020-05-05 17:11:23 -0300

    criada a constante pessoa

--- js/js/revisao.js
+++ js/js/revisao.js
@@ -1,7 +1,8 @@
 document.getElementById('btnInserir').onclick = criarLista;
 
+const lista = [];
+
 function criarLista() {
-    
 
     const pessoa = {
         nome: document.getElementById('nome').value,
@@ -9,6 +10,12 @@ function criarLista() {
         masculino: document.getElementById('masculino').checked,
         feminino: document.getElementById('feminino').checked,
     }
-
-    
 }
+
+
+
+
+/*
+Crie um programa que entre com os dados de altura e sexo de 10 pessoas. 
+percorra a lista e informe quantas pessoas são do sexo masculino e quantas pessoas são do sexo feminino. 
+Mostre também qual é a maior altura entre homens e maior altura entre mulheres.
