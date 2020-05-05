7d602e452dea504913fb1c8cdfc9391b82dd4d07
Aline-Acs <aline_capoani@hotmail.com>
2020-05-05 17:01:25 -0300

    iniciado JS

--- js/js/revisao.js
+++ js/js/revisao.js
@@ -1,1 +1,14 @@
-//teste git hub
+document.getElementById('btnInserir').onclick = criarLista;
+
+function criarLista() {
+    
+
+    const pessoa = {
+        nome: document.getElementById('nome').value,
+        altura: document.getElementById('altura').value,
+        masculino: document.getElementById('masculino').checked,
+        feminino: document.getElementById('feminino').checked,
+    }
+
+    
+}
